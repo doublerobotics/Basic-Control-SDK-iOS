bcec598c86aa42683e3530082a2773e5285dae33
Cameron Aaron <cameronaaron@users.noreply.github.com>
2018-08-13 11:43:35 -0400

    Update DoubleControlSDK.h

--- DoubleControlSDK.framework/Versions/A/Headers/DoubleControlSDK.h
+++ DoubleControlSDK.framework/Versions/A/Headers/DoubleControlSDK.h
@@ -1,4 +1,3 @@
-//
 //  DoubleBasicSDK.h
 //  Double Basic Example
 //
@@ -6,9 +5,6 @@
 //  Copyright (c) 2013 Double Robotics, Inc. All rights reserved.
 //
 
-#ifndef Double_Basic_Example_DoubleBasicSDK_h
-#define Double_Basic_Example_DoubleBasicSDK_h
-
-#import "DRDouble.h"
-
+#if !Double_Basic_Example_DoubleBasicSDK_h
+//#define Double_Basic_Example_DoubleBasicSDK_h
 #endif
